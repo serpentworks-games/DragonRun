41572f24c47f9afb019e083781e5d62098ee1d43
serpentworks-games <alexisbarta@outlook.com>
2018-08-30 18:11:23 -0700

    Update TileManager.cs

--- TileManager.cs
+++ TileManager.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
-
 public class TileManager : MonoBehaviour
 {
 	//Arrays to hold the tile prefabs, assigned in editor
@@ -86,42 +85,39 @@ public class TileManager : MonoBehaviour
 	//Reference to the Game Manager script
 	private GameManager gameManager;
 
-
 	void Start ()
 	{
 		gameManager = FindObjectOfType<GameManager> ();
 		CreateTiles (50);
 		//CreateLargeTiles (0);
 
-		for (int i = 0; i < 30; i++) {
+		for (int i = 0; i < 30; i++) 
+		{
 			SpawnTile ();
-
-		}
-			
+		}		
 	}
 
 	//Creates the Path Tiles
 	public void CreateTiles (int amount)
 	{
-		for (int i = 0; i < amount; i++) {
-			
+		for (int i = 0; i < amount; i++) 
+		{
 			leftTiles.Push (Instantiate (tilesPrefabs [0])); //Left Path Tiles
 			topTiles.Push (Instantiate (tilesPrefabs [1])); //Top Path Tiles
-
 
 			leftTiles.Peek ().name = "LeftTile";
 			topTiles.Peek ().name = "TopTile";
 
 			leftTiles.Peek ().SetActive (false);
 			topTiles.Peek ().SetActive (false);
-
 		}
 	}
 
 	//Creates the Large Tiles
 	public void CreateLargeTiles (int amount)
 	{
-		for (int i = 0; i < amount; i++) {
+		for (int i = 0; i < amount; i++) 
+		{
 
 			lt_TwinHeads.Push (Instantiate (largeTilesPrefabs [0])); //TwinHeads Tile
 			lt_Snowdrifts.Push (Instantiate (largeTilesPrefabs [1])); //Snowdrift Tile
@@ -137,23 +133,24 @@ public class TileManager : MonoBehaviour
 			lt_Snowdrifts.Peek ().SetActive (false);
 			tl_Island.Peek ().SetActive (false);
 			tl_Lava.Peek ().SetActive (false);
-
-
 		}
 	}
 
 	public void SpawnTile ()
 	{
 		//These make sure we always have tiles to pull from, even if they do not get put back into the stack
-		if (leftTiles.Count == 0 || topTiles.Count == 0) {
+		if (leftTiles.Count == 0 || topTiles.Count == 0)
+		{
 			CreateTiles (10);
 		}
 
-		if (lt_TwinHeads.Count == 0 || lt_Snowdrifts.Count == 0) {
+		if (lt_TwinHeads.Count == 0 || lt_Snowdrifts.Count == 0) 
+		{
 			CreateLargeTiles (10);
 		}
 
-		if (tl_Island.Count == 0 || tl_Lava.Count == 0) {
+		if (tl_Island.Count == 0 || tl_Lava.Count == 0) 
+		{
 			CreateLargeTiles (10);
 		}
 
@@ -165,7 +162,8 @@ public class TileManager : MonoBehaviour
 		int randomIndexForLarge = Random.Range (0, 2);
 
 		//Regular sized tiles
-		if (randomIndex == 0) {
+		if (randomIndex == 0) 
+		{
 			GameObject tmp = leftTiles.Pop ();
 			tmp.SetActive (true);
 			tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
@@ -173,9 +171,9 @@ public class TileManager : MonoBehaviour
 
 			//When you spawn a path tile, increase the tile count
 			gameManager.tileCount++;
-
-		} else if (randomIndex == 1) {
-			
+		} 
+		else if (randomIndex == 1) 
+		{	
 			GameObject tmp = topTiles.Pop ();
 			tmp.SetActive (true);
 			tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
@@ -188,84 +186,97 @@ public class TileManager : MonoBehaviour
 		//Generate a number for what stone is spawned on the path tiles
 		int spawnStone = Random.Range (0, 3);
 
-		if (currentTile.GetComponent<Tile> ().stonesList != null) {
-
+		if (currentTile.GetComponent<Tile> ().stonesList != null) 
+		{
 			GameObject stones = currentTile.GetComponent<Tile> ().stonesList;
-			if (spawnStone == 0) {
+			if (spawnStone == 0) 
+			{
 				stones.gameObject.transform.GetChild (0).transform.gameObject.SetActive (true);
-			} else if (spawnStone == 1) {
+			} 
+			else if (spawnStone == 1) 
+			{
 				stones.gameObject.transform.GetChild (1).transform.gameObject.SetActive (true);
-			} else if (spawnStone == 2) {
+			} 
+			else if (spawnStone == 2) 
+			{
 				stones.gameObject.transform.GetChild (2).transform.gameObject.SetActive (true);
-			} else {
-
+			} 
+			else 
+			{
 				// Do nothing!
 			}
-		} else {
-			if (currentTile.transform.childCount == 2) {
+		} 
+		else 
+		{
+			if (currentTile.transform.childCount == 2)
+			{
+			 //TODO: Fix this section apparently this never finishhhhheeedd?????
 			}
 		}
-
-
+		
 		//Large tiles
-		if (gameManager.tileCount > 45 && gameManager.tileCount < 60) {
-			
-			if (randomIndex == 0) {
+		if (gameManager.tileCount > 45 && gameManager.tileCount < 60) 
+		{
+			if (randomIndex == 0) 
+			{
 				// Spawn a "Left" tile
-				if (randomIndexForLarge == 0) {
+				if (randomIndexForLarge == 0) 
+				{
 					//Spawan a Twins Heads Tile
 					GameObject tmp = lt_TwinHeads.Pop ();
 					tmp.SetActive (true);
 					tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
 					currentTile = tmp;
 
-				} else if (randomIndexForLarge == 1) { 
+				} 
+				else if (randomIndexForLarge == 1) 
+				{ 
 					//Spawn a Snowdrifts Tile
 					GameObject tmp = lt_Snowdrifts.Pop ();
 					tmp.SetActive (true);
 					tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
 					currentTile = tmp;
 				}
-
-			} else if (randomIndex == 1) { 
+			} 
+			else if (randomIndex == 1) 
+			{ 
 				// Spawn a "Top" tile
-				if (randomIndexForLarge == 0) {
+				if (randomIndexForLarge == 0) 
+				{
 					//Spawan a Twins Heads Tile
 					GameObject tmp = tl_Island.Pop ();
 					tmp.SetActive (true);
 					tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
 					currentTile = tmp;
-
-				} else if (randomIndexForLarge == 1) { 
+				} 
+				else if (randomIndexForLarge == 1) 
+				{ 
 					//Spawn a Lava Tile
 					GameObject tmp = tl_Lava.Pop ();
 					tmp.SetActive (true);
 					tmp.transform.position = currentTile.transform.GetChild (0).transform.GetChild (randomIndex).position;
 					currentTile = tmp;
 				}
-
 			}
 			// Rest the tile count to 0 after you spawn a large tile
 			gameManager.tileCount = 0;
 		}
-
 		//Generate a number for whether the tile spawns a pick up
 		int spawnPickup = Random.Range (0, 10);
 
-		if (spawnPickup > 0 && spawnPickup < 3) {
-			if (currentTile.transform.childCount > 0) {
-				if (currentTile.GetComponent<Tile> ().pickUp != null) {
+		if (spawnPickup > 0 && spawnPickup < 3) 
+		{
+			if (currentTile.transform.childCount > 0) 
+			{
+				if (currentTile.GetComponent<Tile> ().pickUp != null) 
+				{
 					currentTile.GetComponent<Tile> ().pickUp.SetActive (true);
-				} else {
+				} 
+				else 
+				{
 					Debug.Log ("# of children on this object: " + currentTile.transform.childCount + " | Object: " + currentTile.name);
-				}
-					
+				}	
 			}
 		}
-
-	
-		}
-
-
-
+	}
 }
